6d12cd859dd37d5467d36f9ac539819806c62a9e
Kevin Smith <kevinstewartsmith@gmail.com>
2022-09-12 22:01:02 +0700

    Sixth  heroku commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
