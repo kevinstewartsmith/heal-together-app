21022e9b93ea0d89ed348fa309857c6b9a71efa7
Kevin Smith <kevinstewartsmith@gmail.com>
2022-09-12 21:57:16 +0700

    Fifth heroku commit

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+//   });
   
 
 
