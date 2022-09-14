204a776ce5094ef1ca76965e36d06ef98d70642b
Kevin Smith <kevinstewartsmith@gmail.com>
2022-09-14 14:42:47 +0700

    Create build file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+const path = require('path');
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
-// Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+//Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // app.get("/api", (req, res) => {
 //     res.json({ message: "Hello from server!" });
