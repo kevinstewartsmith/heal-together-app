c1e9a7f3ca97ca5de4f73b59ddbfc7bc41163615
Kevin Smith <kevinstewartsmith@gmail.com>
2022-09-14 13:54:09 +0700

    tenth heroku commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3333;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // app.get("/api", (req, res) => {
 //     res.json({ message: "Hello from server!" });
@@ -14,13 +14,13 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
 // All other GET requests not handled before will return our React app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
-  // app.listen(PORT, () => {
-//   console.log(`Server listening on ${PORT}`);
-// });
-app.listen(process.env.PORT || 3333, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+  app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+// app.listen(process.env.PORT || 3333, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//   });
