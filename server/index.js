b963ac9a8babb4a0d2b64dc5509aac05a08230bf
Kevin Smith <kevinstewartsmith@gmail.com>
2022-09-22 09:48:40 +0700

    Add mongo atlas comment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const path = require('path');
+const bodyParser = require("body-parser")
+const _ = require("lodash")
+const mongoose = require("mongoose")
+
+
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
 //Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+
 
 // app.get("/api", (req, res) => {
 //     res.json({ message: "Hello from server!" });
