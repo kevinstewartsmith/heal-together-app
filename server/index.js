80853f5c3434eb89fce9573b7af29da9d8bf0e0f
Kevin Smith <kevinstewartsmith@gmail.com>
2022-10-17 19:22:04 +0700

    Change genral get method from * to /

--- server/index.js
+++ server/index.js
@@ -129,7 +129,7 @@ const Responses = mongoose.model("response", responseSchema);
 //   });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
