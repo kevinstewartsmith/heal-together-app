ed7d166ad5e0e31679d75a672c14a6b5d2a7615d
Kevin Smith <kevinstewartsmith@gmail.com>
2022-10-26 19:55:58 +0700

    Check if response data is string for recaptcha12.

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -41,7 +41,7 @@ function ContactForm(props) {
         let isHuman = "";
 
         await axios.post("/postRecaptcha", {token})
-        .then(res =>  console.log(res.data))
+        .then(res =>  console.log(typeof(res.data)))
         .catch((error) => {
         console.log(error);
         })
