f88c13d828a78f67b5387ed80cd17254f5d7586f
Kevin Smith <kevinstewartsmith@gmail.com>
2022-10-29 20:32:42 +0700

    Creat Languange mode component17.

--- client/src/components/LanguageMode.jsx
+++ client/src/components/LanguageMode.jsx
@@ -22,9 +22,9 @@ export default function LanguageMode() {
 
     return (
         <div className="language-mode" >
-            <div onClick={ console.log("eng clicked")} style={{ display: 'inline'}} ><h5 style={{textDecoration: 'underline', display: 'inline'}}  >ENG</h5></div>
+            <div style={{ display: 'inline'}} ><h5 style={{textDecoration: 'underline', display: 'inline'}} onClick={ console.log("eng clicked")} >ENG</h5></div>
             <div><h5 style={{display: 'inline'}} >|</h5></div>
-            <div onClick={ console.log("esp clicked")} style={{ display: 'inline'}}><h5 style={{display: 'inline' }}  >ESP</h5></div>
+            <div style={{ display: 'inline'}}><h5 style={{display: 'inline' }} onClick={ console.log("esp clicked")} >ESP</h5></div>
         </div>  
     )
 }
